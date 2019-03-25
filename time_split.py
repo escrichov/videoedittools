d589ec39daf1f18f1dbb91506401b5c4e618765d
Carlos Escriche <escrichov@gmail.com>
2019-03-25 23:41:17 +0100

    Validate time_split input file

--- time_split.py
+++ time_split.py
@@ -1,4 +1,5 @@
 import sys
+import datetime
 from subprocess import call
 
 
@@ -8,6 +9,33 @@ def usage(program_name):
 
     ))
     sys.exit(1)
+
+
+def validate_times(content):
+    timeformat = "%H:%M:%S"
+    for i, time_line in enumerate(content):
+        line_number = i + 1
+        time_line = time_line.strip()
+        times = time_line.split(',')
+        if len(times) != 2:
+            print(f"Invalid stard/end '{time_line}' in line {line_number}. Format must be 00:00:00,00:00:00", file=sys.stderr)
+            sys.exit(1)
+
+        start = times[0]
+        end = times[1]
+        try:
+            datetime.datetime.strptime(start, timeformat)
+        except ValueError:
+            print(f"Invalid start date '{start}' in line {line_number}. Format must be 00:00:00", file=sys.stderr)
+            sys.exit(1)
+
+        try:
+            datetime.datetime.strptime(end, timeformat)
+        except ValueError:
+            print(f"Invalid end date '{start}' in line {line_number}", file=sys.stderr)
+            sys.exit(1)
+
+    return [x.strip().split(',') for x in content]
 
 
 def execute(command):
@@ -32,10 +60,9 @@ if __name__ == "__main__":
 
     with open(input_file) as f:
         content = f.readlines()
-        times = [x.strip().split(',') for x in content]
+        times = validate_times(content)
         for counter, time in enumerate(times):
             output_file = "scene_{}.mp4".format(counter)
             from_time=time[0].strip()
             to_time=time[1].strip()
-
             ffmpeg(input_video, output_file, from_time, to_time)
